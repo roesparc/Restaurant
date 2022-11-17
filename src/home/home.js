2e71dd7592d3c59377a47ef525e612dd6bc8293c
Rodesar <rockoesp@hotmail.com>
2022-11-17 17:56:40 -0600

    Add home module

---
+++ src/home/home.js
@@ -0,0 +1,60 @@
+import homeImg from './img/home-img.jpg';
+
+export default function home() {
+    const home = document.createElement('div');
+    home.classList.add('home');
+
+    const homeTitle = document.createElement('h2');
+    homeTitle.textContent = 'Always fresh and delicious';
+
+    const img = document.createElement('img');
+    img.src = homeImg;
+    img.alt = 'Home Image';
+
+
+    home.appendChild(homeTitle);
+    home.appendChild(img);
+    home.appendChild(hours());
+
+    return home;
+}
+
+function hours() {
+    const hours = document.createElement('div');
+    hours.classList.add('hours');
+
+    const hoursTitle = document.createElement('h3');
+    hoursTitle.textContent = 'Hours';
+
+    const monday = document.createElement('p');
+    monday.textContent = 'Monday: 8am - 5pm';
+
+    const tuesday = document.createElement('p');
+    tuesday.textContent = 'Tuesday: 8am - 5pm';
+
+    const wednesday = document.createElement('p');
+    wednesday.textContent = 'Wednesday: 8am - 5pm';
+
+    const thursday = document.createElement('p');
+    thursday.textContent = 'Thursday: 8am - 5pm';
+
+    const friday = document.createElement('p');
+    friday.textContent = 'Friday: 8am - 5pm';
+
+    const saturday = document.createElement('p');
+    saturday.textContent = 'Saturday: 10am - 4pm';
+
+    const sunday = document.createElement('p');
+    sunday.textContent = 'Sunday: 10am - 4pm';
+
+    hours.appendChild(hoursTitle);
+    hours.appendChild(monday);
+    hours.appendChild(tuesday);
+    hours.appendChild(wednesday);
+    hours.appendChild(thursday);
+    hours.appendChild(friday);
+    hours.appendChild(saturday);
+    hours.appendChild(sunday);
+
+    return hours;
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,76 @@
 import './style.css';
+import ghIcon from './img/gh-icon.png';
+import home from './home/home';
+
+const init = (() => {
+    const content = document.querySelector('#content');
+
+    content.appendChild(createHeader());
+    content.appendChild(createMain());
+    content.appendChild(createFooter());
+})();
+
+function createHeader() {
+    const header = document.createElement('header');
+
+    const restName = document.createElement('h1');
+    restName.textContent = 'Green Pick';
+
+    const headerNav = document.createElement('nav');
+
+    const navUl = document.createElement('ul');
+
+    const liHome = document.createElement('li');
+    liHome.classList.add('home-tab');
+    liHome.textContent = 'Home';
+
+    const liMenu = document.createElement('li');
+    liMenu.classList.add('menu-tab');
+    liMenu.textContent = 'Menu';
+
+    const liContact = document.createElement('li');
+    liContact.classList.add('contact-tab');
+    liContact.textContent = 'Contact';
+
+    header.appendChild(restName);
+    header.appendChild(headerNav);
+    headerNav.appendChild(navUl);
+    navUl.appendChild(liHome);
+    navUl.appendChild(liMenu);
+    navUl.appendChild(liContact);
+
+    return header;
+}
+
+function createMain() {
+    const main = document.createElement('main');
+
+    main.appendChild(home());
+
+    return main;
+}
+
+function createFooter() {
+    const footer = document.createElement('footer');
+
+    const by = document.createElement('div');
+    by.textContent = 'By ';
+
+    const author = document.createElement('a');
+    author.href = 'https://github.com/roesparc/';
+    author.textContent = 'roesparc';
+
+    const ghWrap = document.createElement('a');
+    ghWrap.href = 'https://github.com/roesparc/';
+
+    const ghImg = document.createElement('img');
+    ghImg.src = ghIcon;
+    ghImg.alt = 'GitHub Icon';
+
+    footer.appendChild(by);
+    by.appendChild(author);
+    footer.appendChild(ghWrap);
+    ghWrap.appendChild(ghImg);
+
+    return footer;
+}
